cb3f7577d4f8265da4ebb562b59a6eaa2f679b65
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-10-11 23:02:31 +0530

    init

---
+++ src/main/java/com/rudra/institute/InstituteManagementApplication.java
@@ -0,0 +1,13 @@
+package com.rudra.institute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudra/institute/InstituteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra.institute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
